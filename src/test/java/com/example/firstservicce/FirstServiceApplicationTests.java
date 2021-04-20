6cc55083d8016fdc1a29d2c4064194a44008dd1c
이현국 <hklee@sptek.co.kr>
2021-04-20 16:34:33 +0900

    test application init

---
+++ src/test/java/com/example/firstservicce/FirstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstservicce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
